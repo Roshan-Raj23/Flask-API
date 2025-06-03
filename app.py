c78908167a824190ee43afc6178eed57679ec29a
Roshan-Raj23 <roshanraj1543@gmail.com>
2025-06-04 00:50:46 +0530

    Initial

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask , jsonify
+from flask_cors import CORS
+import yt_dlp
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/<url>' , methods=['GET'])
+def index(url):
+    # url = "https://www.youtube.com/watch?v=WquGhpG5o1Y"
+    url = "https://www.youtube.com/watch?v=" + url
+
+    ydl_opts = { 'quiet': True,'skip_download': True }
+
+    ydl = yt_dlp.YoutubeDL(ydl_opts)
+    info = ydl.extract_info(url, download=False)
+
+    progressive_formats = [
+        f for f in info['formats']
+        if f.get('acodec') != 'none' and f.get('vcodec') != 'none'
+    ]
+
+    best_progressive = max(progressive_formats, key=lambda x: x.get('height', 0))
+
+
+    return jsonify(message=best_progressive['url'])
+
+if __name__ == '__main__':
+    app.run(debug=False)
